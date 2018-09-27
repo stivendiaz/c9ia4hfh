17c7215e5198dd5d0b5f80fa9511605a7235a040
Diaz <david.stiven.diaz@accenture.com>
2018-09-27 17:29:41 -0500

    Validates

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,7 +13,7 @@
 #
 
 class Room < ActiveRecord::Base
-    validates :title,:description,:beds,:guests, presence: true
+    validates :title,:description,:beds,:guests,:image_url, presence: true
     validates :description, length: { maximum: 400 }
     validates :beds, numericality: { only_integer: true }
     validates :guests, numericality: { only_integer: true }
