fc70ca233ddb32236c67eb29a75b633ce3ae1a02
Stiven Diaz <stivendiazagudelo@gmail.com>
2018-09-29 20:18:28 -0500

    Añadimos destroy and edit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,22 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    @room.update(room_params)
+    redirect_to rooms_path
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests,:price_per_night, :image_url)
